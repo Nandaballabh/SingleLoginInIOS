f429e775da2869855aa715c9d44b1c4a80896739
Nanda Ballabh <nanda@monocept.com>
2013-04-15 18:04:54 +0530

    App id changed

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 SingleLoginInIOS
 ================
 
- //  Copyright (c) 2013 Nanda Ballabh. All rights reserved.
+ // Copyright (c) 2013 Nanda Ballabh. All rights reserved.
 
 This project is a simple example of enable single login in iOS apps.
--- SingleLoginInIOS/NBAppDelegate.m
+++ SingleLoginInIOS/NBAppDelegate.m
@@ -4,7 +4,27 @@
 //
 //  Created by Nanda Ballabh on 10/04/13.
 //  Copyright (c) 2013 Nanda Ballabh. All rights reserved.
+
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
 //
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
 
 #import "NBAppDelegate.h"
 #import <FacebookSDK/FBSession.h>
@@ -20,7 +40,7 @@ NSString *const FBSessionStateChangedNotification =NBFBSessionStateChangedNotifi
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    NBViewController *viewController = [[NBViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    NBViewController *viewController = [[NBViewController alloc] initWithNibName:@"NBViewController" bundle:nil];
     self.mainNavigation = [[UINavigationController alloc]initWithRootViewController:viewController];
     self.window.rootViewController = self.mainNavigation;
     [self.window makeKeyAndVisible];
@@ -66,7 +86,7 @@ NSString *const FBSessionStateChangedNotification =NBFBSessionStateChangedNotifi
         case FBSessionStateOpen:
             if (!error)
             {
-                
+            
             }
             break;
             
